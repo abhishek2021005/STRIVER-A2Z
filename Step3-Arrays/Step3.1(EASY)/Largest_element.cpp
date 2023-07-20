9468dca3341673461ab1d86681bcfae8f76e7d10
Abhishek Kumar <abhishek2021005@gmail.com>
2023-07-20 23:24:44 +0530

    Create Largest_element.cpp

---
+++ Step3-Arrays/Step3.1(EASY)/Largest_element.cpp
@@ -0,0 +1,13 @@
+// #GFG QUESTION(BASIC)
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {   int max=arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]>max)max=arr[i];
+        }
+        return max;
+    }
+};
