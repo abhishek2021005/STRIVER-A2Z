05d1f92acab6d20f22bf58405d789d30e6beaa60
Abhishek Kumar <abhishek2021005@gmail.com>
2023-07-21 17:53:50 +0530

    Create Left rotate an array by k places.cpp

---
+++ Step3-Arrays/Step3.1(EASY)/Left rotate an array by k places.cpp
@@ -0,0 +1,37 @@
+#LEETCODE Q189(MEDIUM) + GFG
+
+class Solution {
+public:
+    
+    void reverse(vector <int> &arr, int start, int end){
+        while(start<=end){
+        int temp = arr[start];
+         arr[start++] = arr[end];
+         arr[end--]=temp;}
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n= nums.size();
+        k = k % n;
+        if(n==0 || k==0 || n==k){
+	       return;
+	   }
+//         int temp[k];
+//      for(int i=n-k; i<n; i++){
+//          temp[i-n+k]=nums[i];
+//      }
+//         for(int i=n-k-1; i>=0; i--){
+//             nums[i+k]=nums[i];
+//         }
+//         for(int i=0;i<k;i++){
+//             nums[i]=temp[i];
+//         }
+        
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0, n-1);
+        
+        
+        
+        
+    }
+};
