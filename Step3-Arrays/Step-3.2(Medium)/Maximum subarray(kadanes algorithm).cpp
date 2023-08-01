8b19572c68df64597c939e79b93cfba2c460ea50
Abhishek Kumar <abhishek2021005@gmail.com>
2023-08-02 00:01:59 +0530

    Update Maximum subarray(kadanes algorithm).cpp

--- Step3-Arrays/Step-3.2(Medium)/Maximum subarray(kadanes algorithm).cpp
+++ Step3-Arrays/Step-3.2(Medium)/Maximum subarray(kadanes algorithm).cpp
@@ -1,4 +1,4 @@
-// LEETCODE (Q53 MEDIUM)
+// #LEETCODE (Q53 MEDIUM)
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -9,5 +9,25 @@ public:
             if(sum<0)sum=0;
         
         }return max;
+    }
+};
+
+//#KADANE'S ALGORITHM (MEDIUM)
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        long long sum=0,max=INT_MIN;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            if(sum>max)max=sum;
+            if(sum<0)sum=0;
+            
+        }return max;
+        // Your code here
+        
     }
 };
